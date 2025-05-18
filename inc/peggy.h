31a4dadb32325e4df9f8b42cc921b52cdda9baec
mayh_m 😊 <mayh7m@gmail.com>
2025-05-18 15:26:11 +0200

    cleaning the source code

--- inc/peggy.h
+++ inc/peggy.h
@@ -21,55 +21,77 @@
 
 namespace godot {
 
-    enum SquishAction {
+    // enum for adequate scale change used in squish()
+    enum class SquishAction {
         JUMP,
         LAND,
         CHANGE_DIRECTION
     };
 
+    // the player class
     class Peggy : public CharacterBody2D {
         GDCLASS(Peggy, CharacterBody2D);
 
+        // movement variables for setters/getters
+        float speed = 750.f;
+        float jumpForce = 1500.f;
+        float gravity = 80.f;
+
+        // direction variables used for movement and animations
         float directionX = .0f;
         float oldDirectionX = .0f;
         bool facingRight = true;
-        float speed = 100.f;
-        float jumpForce = 750.f;
-        float gravity = 10.f;
+
+        // variables used for gliding
         bool hasLaunched = false;
         bool airbourne = true;
 
+        // current animation name
+        StringName animation = "glide";
+
+        // rand variables to determine the pitch scale of the sounds
         std::random_device rd;
         std::mt19937 gen;
         std::uniform_real_distribution<> distr;
 
-        StringName animation = "idle";
-
     protected:
+        // obligatory method used for binding methods in the godot editor
         static void _bind_methods();
 
     public:
+        // construcors/destructor
         Peggy();
         ~Peggy() {};
 
+        // methods overrided from the godot classes
         virtual void _ready() override;
-        virtual void _physics_process(double) override;
-        virtual void _process(double) override;
+        virtual void _physics_process(double delta) override;
+        virtual void _process(double delta) override;
 
+        // movement/animation methods
         void getInput();
         void applyGravity();
         void getFacingDirection();
         void getAnimation();
+        void jump();
 
-        void squish(SquishAction);
+        // additional methods for cosmetics
+        void squish(const SquishAction);
+        void unsquish(const double delta);
+        void changeDirectionBodyResponse();
+        void playSound(const NodePath& sound, const double defaultPitchValue);
 
-        void playSfxFootstep();
+        // methods triggered after adequate godot signals
+        void on_animated_sprite_2d_frame_changed();
 
-        void setSpeed(const float);
-        float getSpeed() const;
-        void setJumpForce(const float _jumpForce);
-        float getJumpForce() const;
-        void setGravity(const float _gravity);
-        float getGravity() const;
+        // setters/getters
+        inline void setSpeed(const float _speed) { speed = _speed; }
+        inline float getSpeed() const { return speed; }
+
+        inline void setJumpForce(const float _jumpForce) { jumpForce = _jumpForce; }
+        inline float getJumpForce() const { return jumpForce; }
+
+        inline void setGravity(const float _gravity) { gravity = _gravity; }
+        inline float getGravity() const { return gravity; }
     };
 }
--- src/peggy.cpp
+++ src/peggy.cpp
@@ -4,8 +4,10 @@ using namespace godot;
 
 void Peggy::_bind_methods() {
 
-    ClassDB::bind_method(D_METHOD("playSfxFootstep"), &Peggy::playSfxFootstep);
+    // signals
+    ClassDB::bind_method(D_METHOD("on_animated_sprite_2d_frame_changed"), &Peggy::on_animated_sprite_2d_frame_changed);
 
+    // properties
     ClassDB::bind_method(D_METHOD("getSpeed"), &Peggy::getSpeed);
     ClassDB::bind_method(D_METHOD("setSpeed", "_speed"), &Peggy::setSpeed);
     ClassDB::add_property("Peggy", PropertyInfo(Variant::FLOAT, "speed"), "setSpeed", "getSpeed");
@@ -29,71 +31,46 @@ void Peggy::_ready() {
     else 
         set_process_mode(Node::ProcessMode::PROCESS_MODE_INHERIT);
 
-    set_velocity({0, 0});
+    set_velocity({ 0, 0 }); // to ensure starting velocity is zeroed
 
-    gen = std::mt19937(rd()); // seed the generator
-    distr = std::uniform_real_distribution<>(0.75, 1.25); // define the range
+    gen = std::mt19937(rd());   // seed the generator
+    distr = std::uniform_real_distribution<>(0.75, 1.25);   // define the range
 }
 
 void Peggy::_physics_process(double delta) {
     getInput();
-    applyGravity();
     getFacingDirection();
-    getAnimation();
+
+    applyGravity();
     set_velocity({ directionX * speed, get_velocity()[1] });
-    move_and_slide();
 
+    getAnimation();
+
+    move_and_slide();   // execute the movement
+
+    // landing squish
     if (airbourne && is_on_floor()) {
+        squish(SquishAction::LAND);
+        playSound("Sfx/SfxFall", 2.0);
         airbourne = false;
-        squish(LAND);
-        get_node<AudioStreamPlayer2D>("Sfx/SfxFall")->set_pitch_scale(2.0);
-        get_node<AudioStreamPlayer2D>("Sfx/SfxFall")->set_pitch_scale(get_node<AudioStreamPlayer2D>("Sfx/SfxFall")->get_pitch_scale() * distr(gen));
-        get_node<AudioStreamPlayer2D>("Sfx/SfxFall")->play();
     }
-
 }
 
 void Peggy::_process(double delta) {
-    get_node<AnimatedSprite2D>("AnimatedSprite2D")->set_scale(
-        { static_cast<real_t>(Math::move_toward(static_cast<double>(get_node<AnimatedSprite2D>("AnimatedSprite2D")->get_scale().x), 1.0, 1.5 * delta)), 
-          static_cast<real_t>(Math::move_toward(static_cast<double>(get_node<AnimatedSprite2D>("AnimatedSprite2D")->get_scale().y), 1.0, 1.5 * delta)) }
-    );
+    unsquish(delta);
 }
 
 void Peggy::getInput() {
     directionX = Input::get_singleton()->get_axis("left", "right");
+
+    // executes when the direction (< 0.0, 0.0, > 0.0) has been changed after the input
     if(directionX != oldDirectionX) {
         oldDirectionX = directionX;
-        squish(CHANGE_DIRECTION);
-        if(directionX) {
-            get_node<AudioStreamPlayer2D>("Sfx/SfxChangeDirection")->set_pitch_scale(2.5);
-            get_node<AudioStreamPlayer2D>("Sfx/SfxChangeDirection")->set_pitch_scale(get_node<AudioStreamPlayer2D>("Sfx/SfxChangeDirection")->get_pitch_scale() * distr(gen));
-            get_node<AudioStreamPlayer2D>("Sfx/SfxChangeDirection")->play();
-            if(is_on_floor()) {
-                get_node<AudioStreamPlayer2D>("Sfx/SfxFootstep")->set_pitch_scale(2.0);
-                get_node<AudioStreamPlayer2D>("Sfx/SfxFootstep")->set_pitch_scale(get_node<AudioStreamPlayer2D>("Sfx/SfxFootstep")->get_pitch_scale() * distr(gen));
-                get_node<AudioStreamPlayer2D>("Sfx/SfxFootstep")->play();
-            }
-        }
-        if(!directionX) {
-            if(is_on_floor()) {
-                get_node<AudioStreamPlayer2D>("Sfx/SfxFootstep")->set_pitch_scale(2.0);
-                get_node<AudioStreamPlayer2D>("Sfx/SfxFootstep")->set_pitch_scale(get_node<AudioStreamPlayer2D>("Sfx/SfxFootstep")->get_pitch_scale() * distr(gen));
-                get_node<AudioStreamPlayer2D>("Sfx/SfxFootstep")->play();
-            }
-        }
+        changeDirectionBodyResponse(); 
     }
 
-    if (Input::get_singleton()->is_action_just_pressed("jump") && is_on_floor()) {
-        set_velocity({ get_velocity()[0], -jumpForce });
-        hasLaunched = true;
-        airbourne = true;
-
-        squish(JUMP);
-        get_node<AudioStreamPlayer2D>("Sfx/SfxJump")->set_pitch_scale(1.5);
-        get_node<AudioStreamPlayer2D>("Sfx/SfxJump")->set_pitch_scale(get_node<AudioStreamPlayer2D>("Sfx/SfxJump")->get_pitch_scale() * distr(gen));
-        get_node<AudioStreamPlayer2D>("Sfx/SfxJump")->play();
-    }
+    if (Input::get_singleton()->is_action_just_pressed("jump") && is_on_floor())
+        jump();
 }
 
 void Peggy::applyGravity() {
@@ -101,15 +78,15 @@ void Peggy::applyGravity() {
 }
 
 void Peggy::getFacingDirection() {
-    if (directionX != 0) {
+    if (directionX != 0)
         facingRight = directionX > 0;
-    }
 }
 
 void Peggy::getAnimation() {
 
-    animation = "idle";
+    animation = "idle"; // default animation
 
+    // determine if the character is jumping or has finished and is gliding now
     if (!is_on_floor()) {
         if (hasLaunched) {
             animation = "jump";
@@ -119,58 +96,66 @@ void Peggy::getAnimation() {
         else
             animation = "glide";
     }
+    // else if the chacter is on the ground and is moving, select running animation
     else if (directionX != 0.f) {
         animation = "run";
     }
 
+    get_node<AnimatedSprite2D>("AnimatedSprite2D")->set_flip_h(!facingRight);   // flip sprites adequate to facing direction
     get_node<AnimatedSprite2D>("AnimatedSprite2D")->set_animation(animation);
-    get_node<AnimatedSprite2D>("AnimatedSprite2D")->set_flip_h(!facingRight);
+}
+
+void Peggy::jump() {
+    set_velocity({ get_velocity()[0], -jumpForce });
+
+    squish(SquishAction::JUMP);
+    playSound("Sfx/SfxJump", 1.5);
+
+    hasLaunched = true;
+    airbourne = true;
 }
 
 void Peggy::squish(SquishAction action) {
     switch(action) {
-        case JUMP:
-            get_node<AnimatedSprite2D>("AnimatedSprite2D")->set_scale({ 0.75f, 1.25f });
+        case SquishAction::JUMP:
+            get_node<AnimatedSprite2D>("AnimatedSprite2D")->set_scale({ 0.67f, 1.33f });
         break;
-        case LAND:
-            get_node<AnimatedSprite2D>("AnimatedSprite2D")->set_scale({ 1.25f, 0.75f });
+        case SquishAction::LAND:
+            get_node<AnimatedSprite2D>("AnimatedSprite2D")->set_scale({ 1.33f, 0.67f });
         break;
-        case CHANGE_DIRECTION:
-            get_node<AnimatedSprite2D>("AnimatedSprite2D")->set_scale({ 0.75f, 1.25f });
+        case SquishAction::CHANGE_DIRECTION:
+            get_node<AnimatedSprite2D>("AnimatedSprite2D")->set_scale({ 0.67f, 1.33f });
         break;
         default:
         break;
     }
 }
 
-void Peggy::playSfxFootstep() {
-    if (get_node<AnimatedSprite2D>("AnimatedSprite2D")->get_animation().get_basename() == "run" && get_node<AnimatedSprite2D>("AnimatedSprite2D")->get_frame() == 0) {
-        get_node<AudioStreamPlayer2D>("Sfx/SfxFootstep")->set_pitch_scale(2.0);
-        get_node<AudioStreamPlayer2D>("Sfx/SfxFootstep")->set_pitch_scale(get_node<AudioStreamPlayer2D>("Sfx/SfxFootstep")->get_pitch_scale() * distr(gen));
-        get_node<AudioStreamPlayer2D>("Sfx/SfxFootstep")->play();
+void Peggy::unsquish(const double delta) {
+    get_node<AnimatedSprite2D>("AnimatedSprite2D")->set_scale(
+        { static_cast<real_t>(Math::move_toward(static_cast<double>(get_node<AnimatedSprite2D>("AnimatedSprite2D")->get_scale().x), 1.0, 1.5 * delta)), 
+          static_cast<real_t>(Math::move_toward(static_cast<double>(get_node<AnimatedSprite2D>("AnimatedSprite2D")->get_scale().y), 1.0, 1.5 * delta)) }
+    ); // fix the size a little more to the default sprite scale
+}
+
+void Peggy::changeDirectionBodyResponse() {
+    if(directionX < 0.f && facingRight || directionX > 0.f && !facingRight) {
+        squish(SquishAction::CHANGE_DIRECTION);
+        playSound("Sfx/SfxChangeDirection", 2.5);
     }
+
+    if(is_on_floor())
+        playSound("Sfx/SfxFootstep", 2.0);
 }
 
-void Peggy::setSpeed(const float _speed) {
-    speed = _speed;
+void Peggy::playSound(const NodePath& sound, double defaultPitchValue) {
+    get_node<AudioStreamPlayer2D>(sound)->set_pitch_scale(defaultPitchValue);
+    get_node<AudioStreamPlayer2D>(sound)->set_pitch_scale(get_node<AudioStreamPlayer2D>(sound)->get_pitch_scale() * distr(gen));
+    get_node<AudioStreamPlayer2D>(sound)->play();
 }
 
-float Peggy::getSpeed() const {
-    return speed;
-}
-
-void Peggy::setJumpForce(const float _jumpForce) {
-    jumpForce = _jumpForce;
-}
-
-float Peggy::getJumpForce() const {
-    return jumpForce;
-}
-
-void Peggy::setGravity(const float _gravity) {
-    gravity = _gravity;
-}
-
-float Peggy::getGravity() const {
-    return gravity;
+void Peggy::on_animated_sprite_2d_frame_changed() {
+    // execute when the character is running and is making a step (adequate frame)
+    if (get_node<AnimatedSprite2D>("AnimatedSprite2D")->get_animation().get_basename() == "run" && get_node<AnimatedSprite2D>("AnimatedSprite2D")->get_frame() == 2)
+        playSound("Sfx/SfxFootstep", 2.0);
 }
